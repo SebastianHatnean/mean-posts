cf198859b7c01857de1545ca9d469e0eb9b59515
sebastianhatnean <sebastian.hatnean@gmail.com>
2020-10-01 17:16:01 +0300

    [MEAN-App] small change for images folder

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
+app.use("/images", express.static(path.join("backend/images")));
 
 //CORS
 app.use((req, res, next) => {
